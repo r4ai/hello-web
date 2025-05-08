1f17fb5c5f428d8f8a0816a58fed9306ea011fbb
rai <96561881+r4ai@users.noreply.github.com>
2025-05-08 19:41:17 +0900

    chore: build app and slides

---
+++ .github/scripts/build.ts
@@ -0,0 +1,14 @@
+import { $ } from "bun";
+import * as fs from "node:fs/promises";
+
+const REPO_NAME = "hello-web";
+
+// ensure `dist` directory exists
+await fs.rm("dist", { recursive: true, force: true });
+await fs.mkdir("dist", { recursive: true });
+
+// build the slides
+await $`bun run slidev build --base=/${REPO_NAME}/slides/ --out=dist/slides`;
+
+// build the app
+await $`bun run --cwd=app vite build --base=/${REPO_NAME}/app/ --outDir=../dist/app --emptyOutDir`;
